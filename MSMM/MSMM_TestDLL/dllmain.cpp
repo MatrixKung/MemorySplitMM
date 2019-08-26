57f6b23a85e2b466421088c773d8e3deda56eb53
TrevenT <28998668+TrevenT@users.noreply.github.com>
2019-08-26 11:57:53 +1000

    Hello, World! test DLL

--- MSMM/MSMM_TestDLL/dllmain.cpp
+++ MSMM/MSMM_TestDLL/dllmain.cpp
@@ -1,6 +1,15 @@
 #include <Windows.h>
 #include <iostream>
 
+#pragma code_seg(".test2")
+void UsefullFunction()
+{
+	//auto pString = LoadLibraryA("pString");
+	//std::cout << pString << std::endl;
+	MessageBoxA(NULL, "Hello, World!", "MSMM_TestDLL.dll", NULL);
+}
+
+#pragma code_seg(".test1")
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -9,7 +18,7 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
-		std::cout << "DLL_PROCESS_ATTACH" << std::endl;
+		UsefullFunction();
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
