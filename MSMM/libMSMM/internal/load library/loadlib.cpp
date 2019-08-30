6cc28dd58e779d51000188c1e4a705e54025cb13
TrevenT <28998668+TrevenT@users.noreply.github.com>
2019-08-30 15:36:43 +1000

    Tempary Solution to crt runtime libraries failing to load

--- MSMM/libMSMM/internal/load library/loadlib.cpp
+++ MSMM/libMSMM/internal/load library/loadlib.cpp
@@ -25,31 +25,40 @@ namespace libMSMM::ll
 			return nullptr;
 		}
 
+		std::string ModuleName = pModuleName;
+
+		if (ModuleName.substr(0, 14) == "api-ms-win-crt")
+		{
+			// redirect to c runtime kit path
+			ModuleName = "C:\\Program Files (x86)\\Windows Kits\\10\\Redist\\ucrt\\DLLs\\x86\\" + ModuleName;
+		}
+
 		// Rebase local LoadLibraryA to remote LoadLibraryA
 		uint32_t pLoadLibraryRemote = (uint32_t)LocalLoadLibrary - (uint32_t)LocalKernal32 + (uint32_t)RemoteKernal32;
 		char* ppLLR = (char*)& pLoadLibraryRemote;
 
-		auto DLLNameSize = strlen(pModuleName) + 1;
+		auto DLLNameSize = strlen(ModuleName.c_str()) + 1;
 		auto pRemoteDLLName = Process.AllocateMemory(DLLNameSize, PAGE_READWRITE);
-		Process.WriteMemory((void*)pModuleName, pRemoteDLLName, DLLNameSize);
+		Process.WriteMemory((void*)ModuleName.data(), pRemoteDLLName, DLLNameSize);
 
 		auto Thread = CreateRemoteThread(Process.GetHandle(), NULL, NULL, (LPTHREAD_START_ROUTINE)pLoadLibraryRemote, (LPVOID)pRemoteDLLName, NULL, NULL);
 
 		if (!Thread)
 		{
-			LOG_ERROR("Failed to run remote loadlibrary on {}", pModuleName);
+			LOG_ERROR("Failed to run remote loadlibrary on {}", ModuleName);
 			Process.FreeMemory(pRemoteDLLName);
 			return 0;
 		}
 
 		WaitForSingleObject(Thread, INFINITE);
 
-		Process.FreeMemory(pRemoteDLLName);
+		std::this_thread::sleep_for(0.5s);
 
+		Process.FreeMemory(pRemoteDLLName);
 		auto Module = (HMODULE)Process.GetRemoteModule(pModuleName);
 		if (!Module)
 		{
-			LOG_ERROR("Failed to load module {}: {}", pModuleName, GetLastError());
+			LOG_ERROR("Failed to load module {}: {}", ModuleName, GetLastError());
 			return 0;
 		}
 		return Module;
